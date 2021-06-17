9fbe8e58188e8257257d051c763a3243a7e348d7
Mikhail Kondrashin <mkondrashin@gmail.com>
2021-06-17 13:19:03 +0300

    commit

---
+++ pkg/smartcheck/smartcheck.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	URL := "https://192.168.184.18:31616/get/sessions"
+	fmt.Println("Calling API...")
+
+	requestBody := `{
+  "user": {
+    "userID": "administrator",
+    "password": "Zxcv7890!"
+  }
+}`
+	client := &http.Client{}
+	body := bytes.NewBufferString(requestBody)
+	req, err := http.NewRequest("POST", URL, body)
+	if err != nil {
+		fmt.Print(err.Error())
+		return
+	}
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	fmt.Print(string(bodyBytes))
+	//	var responseObject Response
+	//	json.Unmarshal(bodyBytes, &responseObject)
+	//	fmt.Printf("API Response as struct %+v\n", responseObject)
+
+}
