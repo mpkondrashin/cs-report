7f6a53c3e99cec66ecc582273c7f585381093b80
Mikhail Kondrashin <mkondrashin@gmail.com>
2021-06-19 20:46:08 +0300

    commit

--- pkg/smartcheck/struct.go
+++ pkg/smartcheck/struct.go
@@ -5,17 +5,22 @@ import (
 	"reflect"
 )
 
-func StructCSV(data interface{}) (name []string, value []string) {
-	return structCSV(data, "")
+func StructCSV(structPtr interface{}) (name []string, value []string) {
+	return structCSV(structPtr, "")
 }
 
-func structCSV(data interface{}, prefix string) (name []string, value []string) {
+func structCSV(structPtr interface{}, prefix string) (name []string, value []string) {
 	name = make([]string, 0)
 	value = make([]string, 0)
 	//structPtrValue := reflect.ValueOf(structPtr)
 	//structValue := reflect.Indirect(structPtrValue)
-	structValue := reflect.ValueOf(data)
+	structValue := reflect.ValueOf(structPtr)
+	if structValue.Kind() == reflect.Ptr {
+		structValue = reflect.Indirect(structValue)
+	}
+	//structValue := reflect.ValueOf(data)
 	structValueType := structValue.Type()
+	//fmt.Println("structCSV", structValue, structValueType)
 	for i := 0; i < structValue.NumField(); i++ {
 		fieldType := structValueType.Field(i)
 		fieldValue := structValue.Field(i)
@@ -24,18 +29,18 @@ func structCSV(data interface{}, prefix string) (name []string, value []string) 
 			fieldName = fmt.Sprintf("%s.%s", prefix, fieldType.Name)
 		}
 		if fieldValue.Type().Kind() == reflect.Struct {
-			subName, subValue := structCSV(fieldValue.Interface(), fieldName)
+			subName, subValue := structCSV(fieldValue.Interface(), prefix)
 			name = append(name, subName...)
 			value = append(value, subValue...)
 		} else {
 			name = append(name, fieldName)
+			//	fmt.Println("name = ", name)
 			value = append(value, fmt.Sprintf("%v", fieldValue.Interface()))
 		}
 	}
 	return
 }
 
-/*
 type S struct {
 	A string
 	B struct {
@@ -49,8 +54,7 @@ func main() {
 	s.A = "AD"
 	s.B.X = 2
 	s.B.Y = 3
-	name, value := StructCSV(s, "")
+	name, value := StructCSV(&s)
 	fmt.Println(name)
 	fmt.Println(value)
 }
-*/
