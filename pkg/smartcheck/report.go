6c02cf07b209abd7727cc6d7dbda69993325196c
Mikhail Kondrashin <mkondrashin@gmail.com>
2021-06-21 11:50:58 +0300

    commit

--- pkg/smartcheck/report.go
+++ pkg/smartcheck/report.go
@@ -16,7 +16,7 @@ type (
 	SmartCheckReport struct {
 		GeneratedOn time.Time
 		Completed   time.Time
-		Difference  time.Time
+		Difference  time.Duration
 		Registries  []RegistryReport
 	}
 	RegistryReport struct {
